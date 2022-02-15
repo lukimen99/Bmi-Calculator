2c1f6a12f8c11697e876f5d9a3680dc2b41b0156
Łukasz Sobolewski <lukasz.sobolewskiii@gmail.com>
2022-02-15 23:15:04 +0100

    Apka

---
+++ app/src/main/java/com/example/bmi_calculator/About.java
@@ -0,0 +1,41 @@
+package com.example.bmi_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class About extends AppCompatActivity {
+
+    Button gitlink = findViewById(R.id.github);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        gitlink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl();
+            }
+        });
+
+        TextView link = findViewById(R.id.github);
+        String linkText = link.getText() + "<a href='https://github.com/mirzafrina/BMI_Calculator.git";
+        link.setText(Html.fromHtml(linkText));
+        link.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    private void gotoUrl(){
+        Uri uri = Uri.parse("https://github.com/mirzafrina/BMI_Calculator.git");
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+
+    }
+}
